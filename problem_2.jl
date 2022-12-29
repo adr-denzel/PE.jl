e87364fc52fd89ed78084714216fe5f03156d979
Denzel Ramsbottom <46371902+adr-denzel@users.noreply.github.com>
2022-12-29 21:18:47 +0200

    p2 upload

---
+++ problem_2.jl
@@ -0,0 +1,23 @@
+# problem definition: https://projecteuler.net/problem=2
+
+function fib(n)
+	n==1 && return 1
+	n==2 && return 2
+	return fib(n-1) + fib(n-2)
+end
+
+function even_fib_sum(n)
+	index = 1
+	current = 0
+	sum = 0
+	while current < n
+		current = fib(index)
+		if current % 2 == 0
+			sum += current
+		end
+        index += 1
+	end
+	return sum
+end
+
+println(even_fib_reduction(4000000))
