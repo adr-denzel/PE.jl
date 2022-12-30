9ee0dcefdbdafc26b26456ec1e057d707df77c2d
Denzel Ramsbottom <46371902+adr-denzel@users.noreply.github.com>
2022-12-30 18:36:10 +0200

    Update problem_02.jl

--- problem_02.jl
+++ problem_02.jl
@@ -1,23 +1,11 @@
 # problem definition: https://projecteuler.net/problem=2
 
-function fib(n)
-	n==1 && return 1
-	n==2 && return 2
-	return fib(n-1) + fib(n-2)
-end
-
 function even_fib_sum(n)
-	index = 1
-	current = 0
-	sum = 0
-	while current < n
-		current = fib(index)
-		if current % 2 == 0
-			sum += current
-		end
-        index += 1
+	arr = [1,2]
+	while sum(arr[end-1:end]) < n
+		push!(arr, sum(arr[end-1:end]))
 	end
-	return sum
+	return mapreduce(x -> iseven(x) ? x : 0, +, arr)
 end
 
 println(even_fib_sum(4000000))
