74e697437a0564e687b981d45cc1b38407096f91
Denzel Ramsbottom <46371902+adr-denzel@users.noreply.github.com>
2022-12-30 20:46:47 +0200

    Update problem_14.jl

--- problem_14.jl
+++ problem_14.jl
@@ -3,7 +3,7 @@
 function collatz_length(n)
 	distance = 1
 	while n != 1
-		if n % 2 == 0
+		if iseven(n)
 			n = n / 2
 		else
 			n = 3n + 1
@@ -15,8 +15,7 @@ end
 
 function longest_collatz_sequence(n)
 	collatz_lengths = map(collatz_length, collect(1:(n-1)))
-	max = maximum(collatz_lengths)
-	return findfirst(isequal(max), collatz_lengths)
+	return findfirst(isequal(maximum(collatz_lengths)), collatz_lengths)
 end
 
-println(longest_collatz_sequence(1000000))
+println(longest_collatz_sequence(100000))
