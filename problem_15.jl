f29973e8f87a4db382ea2c52c26fcbc9dd17415c
Denzel Ramsbottom <46371902+adr-denzel@users.noreply.github.com>
2022-12-30 14:53:14 +0200

    Delete problem_15.jl

--- problem_15.jl
+++
@@ -1,9 +0,0 @@
-# problem definition: https://projecteuler.net/problem=15
-
-function lattice_path_count(k, n)
-	x = collect(BigInt, n+1:k+n)
-	y = collect(BigInt, 1:n)
-	return reduce(*, x)/reduce(*, y)
-end
-
-println(lattice_path_count(20,20))
