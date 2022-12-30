40ef831f6575f50f85fe8e09cc1cd67261e88153
Denzel Ramsbottom <46371902+adr-denzel@users.noreply.github.com>
2022-12-30 18:44:17 +0200

    Update problem_01.jl

--- problem_01.jl
+++ problem_01.jl
@@ -1,12 +1,3 @@
 # problem definition: https://projecteuler.net/problem=1
 
-function multiple_of_3_or_5(x)
-    if x % 3 == 0 || x % 5 == 0
-        return x
-    else
-        return 0
-    end
-end
-
-n = mapreduce(multiple_of_3_or_5, +, collect(1:999))
-println(n)
+println(mapreduce(x -> (x % 3 == 0 || x % 5 == 0) ? x : 0, +, collect(1:999)))
